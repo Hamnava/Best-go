64515f46b1e9c238fda74d1bf52211ee9f77a491
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-03-16 21:30:10 +0430

    repository created for product

---
+++ Hamnava.Core/Repository/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamnava.Core.Repository.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetAllProductAsync();
+    }
+}
